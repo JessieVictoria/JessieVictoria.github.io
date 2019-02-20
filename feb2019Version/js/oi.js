c12d947f67daa347a1b971940eb3d366635ffd13
Jessie Victoria <hellojessievictoria@gmail.com>
2019-02-20 14:52:13 -0400

    moving new version into repo
    it's called feb2019Version

---
+++ feb2019Version/js/oi.js
@@ -0,0 +1,20 @@
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password (you will be taken to another page)',' ');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "seesomegames") {
+alert('That was it!');
+window.open('aA1Bsd4EQ77gh.html');
+break;
+} 
+testV+=1;
+var pass1 = 
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+history.go(-1);
+return " ";
+} 
+
